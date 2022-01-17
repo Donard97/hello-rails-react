998cdb4cb9e58415814d00c56dd64348f5013a15
Donard97 <donardgola@gmail.com>
2022-01-17 23:44:08 +0100

    Generate React components

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     get 'greeting', to: 'greetings#index'
   end
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
