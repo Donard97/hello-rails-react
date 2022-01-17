03d5df7f11de9630989de8720fbf190957e4ca5b
Donard97 <donardgola@gmail.com>
2022-01-17 23:26:38 +0100

    Genereta Greetings controller for our rails API

---
+++ app/controllers/api/greetings_controller.rb
@@ -0,0 +1,6 @@
+class Api::GreetingsController < ApplicationController
+  def index
+    greeting = Greeting.order(Arel.sql('RANDOM()')).first
+    render json: { greeting: greeting.greeting }
+  end
+end
