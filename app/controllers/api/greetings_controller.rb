af02027bbd33e903b815e5b83821897b32183549
Donard97 <donardgola@gmail.com>
2022-01-17 23:34:52 +0100

    Update routes and generate Static controller

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,4 +1,4 @@
-class Api::GreetingsController < ApplicationController
+class API::GreetingsController < ApplicationController
   def index
     greeting = Greeting.order(Arel.sql('RANDOM()')).first
     render json: { greeting: greeting.greeting }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'greetings/index'
+  get 'static/index'
+  namespace :api, defaults: { format: 'json' } do
+    get 'greeting', to: 'greetings#index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
