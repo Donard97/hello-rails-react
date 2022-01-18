d57ab11841b2fd6a209112926448991dbba77eac
Donard97 <donardgolaj@gmail.com>
2022-01-18 14:33:46 +0100

    Fix rubocop linters

--- test/controllers/greetings_controller_test.rb
+++ test/controllers/greetings_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::GreetingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get greetings_index_url
     assert_response :success
   end
